1ea6dd906a7b6497e95413b75f52a66fbce52272
Alan Navai <alannavai99@gmail.com>
2021-12-25 16:20:31 -0800

    Change message when score is 0

--- script.js
+++ script.js
@@ -8,18 +8,39 @@ document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
-  // 0 is a falsy value -> evaluated as false
+  // When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "No number!";
-  } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Too high!";
-    score--;
-    document.querySelector(".score").textContent = score;
-  } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "Too low!";
-    score--;
-    document.querySelector(".score").textContent = score;
-  } else {
+  }
+
+  // When guess is too high
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      score--;
+      document.querySelector(".score").textContent = score;
+      document.querySelector(".message").textContent = "You lost the game.";
+    }
+  }
+
+  // When guess is too low
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      score--;
+      document.querySelector(".score").textContent = score;
+      document.querySelector(".message").textContent = "You lost the game.";
+    }
+  }
+
+  // When guess is correct
+  else {
     document.querySelector(".message").textContent = "Correct!";
   }
 });
