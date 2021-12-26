d52c80c93548a3d336d4669424e07491e036a1e0
Alan Navai <alannavai99@gmail.com>
2021-12-25 16:08:04 -0800

    Store score inside of variable

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // Generates random number between 1 and upper limit
 const upperLimit = 20;
 const secretNumber = Math.trunc(Math.random() * upperLimit) + 1;
+let score = upperLimit;
 document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -12,8 +13,12 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "No number!";
   } else if (guess > secretNumber) {
     document.querySelector(".message").textContent = "Too high!";
+    score--;
+    document.querySelector(".score").textContent = score;
   } else if (guess < secretNumber) {
     document.querySelector(".message").textContent = "Too low!";
+    score--;
+    document.querySelector(".score").textContent = score;
   } else {
     document.querySelector(".message").textContent = "Correct!";
   }
